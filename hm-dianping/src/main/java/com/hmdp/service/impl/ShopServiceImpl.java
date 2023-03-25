8da6b051c6f59fb2fae0fe4bf9ea10750eb99fb5
stone <330079598@qq.com>
2023-03-25 22:27:39 +0800

    :zap: 缓存击穿问题

--- hm-dianping/src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ hm-dianping/src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -1,13 +1,11 @@
 package com.hmdp.service.impl;
 
-import cn.hutool.json.JSONUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hmdp.dto.Result;
 import com.hmdp.entity.Shop;
 import com.hmdp.mapper.ShopMapper;
 import com.hmdp.service.IShopService;
-import jodd.util.StringUtil;
+import com.hmdp.utils.CacheClient;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,7 +13,8 @@ import org.springframework.transaction.annotation.Transactional;
 import javax.annotation.Resource;
 import java.util.concurrent.TimeUnit;
 
-import static com.hmdp.utils.RedisConstants.*;
+import static com.hmdp.utils.RedisConstants.CACHE_NULL_TTL;
+import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
 
 /**
  * <p>
@@ -30,29 +29,38 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
 
     @Resource
     private StringRedisTemplate stringRedisTemplate;
+    @Resource
+    private CacheClient cacheClient;
 
     @Override
     public Result queryById(Long id) {
-        ObjectMapper objectMapper = new ObjectMapper();
-        // 从Redis查询商铺缓存
-        String cacheShop = stringRedisTemplate.opsForValue().get(CACHE_SHOP_KEY + id);
-        if (StringUtil.isNotBlank(cacheShop)) {
-            Shop shop = JSONUtil.toBean(cacheShop, Shop.class);
-            return Result.ok(shop);
-        }
-        if (cacheShop != null) {
-            return Result.fail("店铺信息不存在！");
-        }
-        // 不存在查询数据库
-        Shop shop = getById(id);
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        // 从Redis查询商铺缓存
+//        String cacheShop = stringRedisTemplate.opsForValue().get(CACHE_SHOP_KEY + id);
+//        if (StringUtil.isNotBlank(cacheShop)) {
+//            Shop shop = JSONUtil.toBean(cacheShop, Shop.class);
+//            return Result.ok(shop);
+//        }
+//        if (cacheShop != null) {
+//            return Result.fail("店铺信息不存在！");
+//        }
+//        // 不存在查询数据库
+//        Shop shop = getById(id);
+//        if (shop == null) {
+//            // 将空值写入到redis当中（解决缓存穿透问题）
+//            stringRedisTemplate.opsForValue().set(CACHE_SHOP_KEY + id, "", CACHE_NULL_TTL, TimeUnit.MINUTES);
+//            return Result.fail("店铺不存在！");
+//        }
+//        // 写入到Redis
+//        stringRedisTemplate.opsForValue().set(CACHE_SHOP_KEY + id, JSONUtil.toJsonStr(shop), CACHE_SHOP_TTL, TimeUnit.MINUTES);
+//        return Result.ok(shop);
+        // 解决缓存穿透
+        Shop shop = cacheClient.
+                queryWithPassThrough(CACHE_SHOP_KEY, id, Shop.class, this::getById, CACHE_NULL_TTL, TimeUnit.MINUTES);
         if (shop == null) {
-            // 将空值写入到redis当中（解决缓存穿透问题）
-            stringRedisTemplate.opsForValue().set(CACHE_SHOP_KEY + id, "", CACHE_NULL_TTL, TimeUnit.MINUTES);
             return Result.fail("店铺不存在！");
         }
-        // 写入到Redis
-        stringRedisTemplate.opsForValue().set(CACHE_SHOP_KEY + id, JSONUtil.toJsonStr(shop), CACHE_SHOP_TTL, TimeUnit.MINUTES);
-        return Result.ok(shop);
+        return Result.ok();
     }
 
     @Override
--- hm-dianping/src/main/java/com/hmdp/utils/RedisConstants.java
+++ hm-dianping/src/main/java/com/hmdp/utils/RedisConstants.java
@@ -9,4 +9,6 @@ public class RedisConstants {
     public static final Long CACHE_NULL_TTL = 2L;
     public static final String CACHE_SHOP_KEY = "cache:shop:";
     public static final String CACHE_SHOP_TYPE = "cache:type:";
+    public static final String LOCK_SHOP_KEY = "lock:shop:";
+    public static final Long LOCK_SHOP_TTL = 10L;
 }
