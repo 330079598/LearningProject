91bb2043e5b538e5d5cf56d7c8b118d9742c67d2
stone <330079598@qq.com>
2023-04-09 22:45:49 +0800

    :sparkles: 导入坐标数据到Redis

--- hm-dianping/src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ hm-dianping/src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -1,9 +1,45 @@
 package com.hmdp;
 
+import com.hmdp.entity.Shop;
+import com.hmdp.service.IShopService;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.geo.Point;
+import org.springframework.data.redis.connection.RedisGeoCommands;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @SpringBootTest
 class HmDianPingApplicationTests {
+    @Resource
+    private IShopService shopService;
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
 
+    @Test
+    void loadShopData() {
+        // 查询店铺信息
+        List<Shop> shops = shopService.list();
+        // 把店铺分组，按照typeId分组，typeId一致的放在一个集合中
+        Map<Long, List<Shop>> collect = shops.stream().collect(Collectors.groupingBy(Shop::getTypeId));
+        // 分批完成写入到Redis
+        for (Map.Entry<Long, List<Shop>> entry : collect.entrySet()) {
+            // 获取类型id
+            Long typeId = entry.getKey();
+            String key = "shop:geo:" + typeId;
+            // 获取同类型的店铺的集合
+            List<Shop> value = entry.getValue();
+            List<RedisGeoCommands.GeoLocation<String>> locations = new ArrayList<>(value.size());
+            for (Shop shop : value) {
+                locations.add(new RedisGeoCommands.GeoLocation<>(String.valueOf(shop.getId()), new Point(shop.getX(), shop.getY())));
+            }
+            stringRedisTemplate.opsForGeo().add(key, locations);
+        }
+    }
 
 }
